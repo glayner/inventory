7bbe23beb5a67d0a3c17c3b62cfb3b9b4959fd06
Thiago Glayner <thiagoglayner_tg@hotmail.com>
2021-05-08 15:26:54 -0300

    remove template data

--- src/modules/manage/infra/http/validations/FindForgotDataByLoginValidate.ts
+++
@@ -1,9 +0,0 @@
-import { celebrate, Segments, Joi } from 'celebrate';
-
-const FindForgotDataByLoginValidate = celebrate({
-  [Segments.PARAMS]: {
-    login: Joi.string().required(),
-  },
-});
-
-export default FindForgotDataByLoginValidate;
